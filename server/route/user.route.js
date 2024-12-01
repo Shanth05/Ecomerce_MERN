61eb8821a3a7ec0511612dca1af5d71ac5eb8508
Shanth05 <shanth.pirashanth5@gmail.com>
2024-12-01 15:57:32 +0530

    Refresh Token Api

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js';
-import { forgotPasswordController, loginController, logoutController, registerUserController, resetPassword, updateUserDetails, uploadAvatar, verifyEmailController, verifyForgotPasswordOtp } from '../controllers/user.controller.js'
+import { forgotPasswordController, loginController, logoutController, refreshToken, registerUserController, resetPassword, updateUserDetails, uploadAvatar, verifyEmailController, verifyForgotPasswordOtp } from '../controllers/user.controller.js'
 import upload from '../middleware/multer.js';
 
 const userRouter = Router()
@@ -14,7 +14,7 @@ userRouter.post('/update-user', auth,updateUserDetails)
 userRouter.put('/forgot-password',forgotPasswordController)
 userRouter.put('/verify-forgot-password-otp',verifyForgotPasswordOtp)
 userRouter.put('/reset-password',resetPassword)
-
+userRouter.post('/refresh-token',refreshToken)
 
 
 export default userRouter
