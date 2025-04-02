3181a24ed652c2ccae1f42344ae5acc4dbaf5574
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-02 22:55:47 +0530

    Update header.jsx

---
+++ client/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='h-20 shadow-md sticky top-0'>
+            {/**logo*/}
+
+             {/**Search */}
+        </header>
+    )
+}
+
+export default Header
