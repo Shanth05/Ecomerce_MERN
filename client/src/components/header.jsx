a27de490b831f84c38315d6c8aaff765da245281
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-09 22:57:21 +0530

    Update header.jsx

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,8 +4,19 @@ const Header = () => {
     return (
         <header className='h-20 shadow-md sticky top-0'>
             {/**logo*/}
-
+            <div>
+                <div>
+                    <img 
+                    src={logo}
+                    width={120}
+                    height={60}
+                    alt='logo'
+                />
+                </div>
+            </div>
              {/**Search */}
+
+             {/**login and my cart */}
         </header>
     )
 }
