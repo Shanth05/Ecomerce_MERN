59b5f0917f33f040c53200d2004c4e8e7dfff512
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-10 22:44:32 +0530

    Update header.jsx

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,20 +3,22 @@ import React from "react";
 const Header = () => {
     return (
         <header className='h-20 shadow-md sticky top-0'>
-            {/**logo*/}
-            <div>
-                <div>
-                    <img 
-                    src={logo}
-                    width={120}
-                    height={60}
-                    alt='logo'
-                />
-                </div>
-            </div>
-             {/**Search */}
+            <div className="container mx-auto items-center h-full">
+                    {/**logo*/}
+                    <div className="h-full">            
+                        <div className="h-full bg-red-500 flex justify-center items-center">
+                            <img 
+                            src={logo}
+                            width={120}
+                            height={60}
+                            alt='logo'
+                        />
+                        </div>
+                    </div>
+                    {/**Search */}
 
-             {/**login and my cart */}
+                    {/**login and my cart */}
+            </div>
         </header>
     )
 }
