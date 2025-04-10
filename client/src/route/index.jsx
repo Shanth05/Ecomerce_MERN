ed7e3b3cdc6b0c9e086c488a78280d79c9d69c76
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-10 22:55:20 +0530

    Update index.jsx

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App.jsx';
+import Home from '../pages/Home.jsx';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children : [
+        {
+            path: "",
+            element: <Home/>
+        }
+    ]
   },
 ]);
